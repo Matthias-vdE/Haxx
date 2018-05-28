d1ad5a193a4c252925ec387948e395808ecaa32d
Matthias vdE <matthias.vandenelsacker@gmail.com>
2018-05-28 20:09:58 +0200

    Pasted Hello World files

---
+++ HelloWorld/HelloWorld.ps1
@@ -0,0 +1,10 @@
+﻿#Load the XML file
+.\loadDialog.ps1 -XamlPath "HelloWorld.xaml"
+
+#Event handler
+$btnHello.add_Click({
+ $lblHello.Content = "Hello World"
+})
+
+#Show the GUI
+$xamGUI.ShowDialog() | out-null
