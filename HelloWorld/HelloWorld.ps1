e2809f94ec7b8c27e4c824c82b1397f157708f3a
Matthias vdE <matthias.vandenelsacker@gmail.com>
2018-05-28 21:23:28 +0200

    Initial SysInfo functions

--- HelloWorld/HelloWorld.ps1
+++ HelloWorld/HelloWorld.ps1
@@ -1,7 +1,7 @@
 ﻿#Load the XML file
 .\loadDialog.ps1 -XamlPath "HelloWorld.xaml"
 
-#Event handler
+#Event handlers
 $btnHello.add_Click({
  $lblHello.Content = "Hello World"
 })
--- SysInfo/SysInfo.ps1
+++ SysInfo/SysInfo.ps1
@@ -1,1 +1,52 @@
-﻿
+﻿#Load the XML file
+.\loadDialog.ps1 -XamlPath "SysInfo.xaml"
+
+#Event handlers
+$frmSysInfo.add_Loaded({
+	LoadOSName
+	LoadOSVersion
+	LoadOSManufacturer
+	LoadSystemName
+	LoadSystemManufacturer
+	LoadSystemSKU
+})
+
+#Functions
+Function LoadOSName {
+	$OSWmiObject = Get-WmiObject -Class Win32_OperatingSystem
+	$OSCaption = $OSWmiObject.Caption
+	$txtOSName.Text = $OSCaption
+}
+
+Function LoadOSVersion {
+	$OSWmiObject = Get-WmiObject -Class Win32_OperatingSystem
+	$OSVersion = $OSWmiObject.Version
+	$txtOSVersion.Text = $OSVersion
+}
+
+Function LoadOSManufacturer {
+	$OSWmiObject = Get-WmiObject -Class Win32_OperatingSystem
+	$OSManufacturer = $OSWmiObject.Manufacturer
+	$txtOSManufacturer.Text = $OSManufacturer
+}
+
+Function LoadSystemName {
+	$SystemWmiObject = Get-WmiObject -Class Win32_ComputerSystem
+	$SystemName = $SystemWmiObject.Name
+	$txtSystemName.Text = $SystemName
+}
+
+Function LoadSystemManufacturer {
+	$SystemWmiObject = Get-WmiObject -Class Win32_ComputerSystem
+	$SystemManufacturer = $SystemWmiObject.Manufacturer
+	$txtSystemManufacturer.Text = $SystemManufacturer
+}
+
+Function LoadSystemSKU {
+	$SystemWmiObject = Get-WmiObject -Class Win32_ComputerSystem
+	$SystemSKU = $SystemWmiObject.SystemSKUNumber
+	$txtSystemSKU.Text = $SystemSKU
+}
+
+#Show the GUI
+$xamGUI.ShowDialog() | out-null
