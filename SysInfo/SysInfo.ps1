cbc78ca7a0d4eb286a3e8f81b3599a80134a746f
Matthias-vdE <matthias.vandenelsacker@gmail.com>
2018-05-30 20:36:38 +0200

    Final code.

--- SysInfo/SysInfo.ps1
+++ SysInfo/SysInfo.ps1
@@ -2,6 +2,7 @@
 $OSWmiObject = Get-WmiObject -Class Win32_OperatingSystem
 $SystemWmiObject = Get-WmiObject -Class Win32_ComputerSystem
 $ProcessorWmiObject = Get-WmiObject -Class Win32_Processor
+$BiosWmiObject = Get-WmiObject -Class Win32_Bios
 
 #Load the XML file
 & "$PSScriptRoot\loadDialog.ps1" -XamlPath "$PSScriptRoot\SysInfo.xaml"
@@ -15,6 +16,11 @@ $frmSysInfo.add_Loaded({
 	LoadSystemManufacturer
 	LoadSystemSKU
 	LoadSystemModel
+	LoadProcessor
+	LoadBios
+	LoadBiosManufacturer
+	LoadPSVersion
+	LoadIEVersion
 })
 
 #Functions
@@ -44,6 +50,31 @@ Function LoadSystemSKU {
 
 Function LoadSystemModel {
 	$txtModel.Text = $SystemWmiObject.Model
+}
+
+Function LoadProcessor {
+	$FullName = $ProcessorWmiObject.Name
+	$Fullname = $FullName -replace "\(R\)", "" -replace "\(TM\)", ""
+	$Split = $FullName -split "@"
+	$ProcName = $Split[0].Trim()
+	$txtProcessor.Text = $ProcName
+}
+
+Function LoadBios {
+	$txtBios.Text = $BiosWmiObject.SMBIOSBIOSVersion
+}
+
+Function LoadBiosManufacturer {
+	$txtBiosManufacturer.Text = $BiosWmiObject.Manufacturer
+}
+
+Function LoadPSVersion {
+	$txtPowershellVersion.Text = $PSVersionTable.PSVersion
+}
+
+Function LoadIEVersion {
+	$IEProperties = Get-Item "$env:ProgramFiles\Internet Explorer\iexplore.exe"
+	$txtIEVersion.Text = $IEProperties.VersionInfo.ProductVersion
 }
 
 #Show the GUI
