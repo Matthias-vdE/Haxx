4422bc8c7e6538b72bdb228babba8a4c7aaedc10
Matthias-vdE <matthias.vandenelsacker@gmail.com>
2018-05-30 21:51:25 +0200

    Created TicTacToe Project

---
+++ TicTacToe/TicTacToe.ps1
@@ -0,0 +1,13 @@
+﻿#Imports and global variables
+
+#Load the XML file
+& "$PSScriptRoot\loadDialog.ps1" -XamlPath "$PSScriptRoot\TicTacToe.xaml"
+
+#Event handlers
+$frmTicTacToe.add_Loaded({
+})
+
+#Functions
+
+#Show the GUI
+$xamGUI.ShowDialog() | out-null
