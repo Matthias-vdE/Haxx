c565e8c7a3a728e1f05002e352bb85937d8411e7
Matthias-vdE <matthias.vandenelsacker@gmail.com>
2018-06-13 21:46:18 +0200

    Initial tests. Time for bed, lol.

---
+++ Snake/Snake.ps1
@@ -0,0 +1,17 @@
+﻿#Imports and global variables
+
+#Load the XML file
+& "$PSScriptRoot\loadDialog.ps1" -XamlPath "$PSScriptRoot\Snake.xaml"
+
+#Event handlers
+$frmSnake.add_Loaded({
+})
+
+$frmSnake.add_PreviewKeyDown({
+	Write-Host "Break here"
+})
+
+#Functions
+
+#Show the GUI
+$xamGUI.ShowDialog() | out-null
