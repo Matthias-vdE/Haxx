d5ff3f4286ec2746e1d59d6443a44819c8c1f908
Matthias-vdE <matthias.vandenelsacker@gmail.com>
2018-06-15 22:11:22 +0200

    Created and tested Snake Class

--- Snake/Snake.ps1
+++ Snake/Snake.ps1
@@ -1,17 +1,68 @@
-﻿#Imports and global variables
+﻿#region Imports and global variables
 
-#Load the XML file
-& "$PSScriptRoot\loadDialog.ps1" -XamlPath "$PSScriptRoot\Snake.xaml"
+	#Set Debugging
+	$DebugPreference = "Continue"
 
-#Event handlers
-$frmSnake.add_Loaded({
-})
+	#Load Windows.Forms. Needed to display Message Boxes
+	[System.Reflection.Assembly]::LoadWithPartialName(“System.Windows.Forms”)
 
-$frmSnake.add_PreviewKeyDown({
-	Write-Host "Break here"
-})
+	 #Import the Snake class file.
+	. "$PSScriptRoot\Snake_class.ps1"
 
-#Functions
+	#Set the global GameSnake variable
+	$Global:GameSnake
 
-#Show the GUI
+#endregion
+
+#region Load the XML file
+
+	& "$PSScriptRoot\loadDialog.ps1" -XamlPath "$PSScriptRoot\Snake.xaml"
+
+#endregion
+
+#region Event handlers
+
+	$frmSnake.add_Loaded({
+		NewGameSnake
+	})
+
+	$frmSnake.add_KeyDown({
+		ChangeDirection($_.Key)
+	})
+
+#endregion
+
+#region Functions
+
+	Function NewGameSnake {
+		$Global:GameSnake = [Snake]::new()
+	}
+
+	Function ChangeDirection {
+		param(
+			[string]$Key
+		)
+		Switch($Key) {
+			"Up" {$Global:GameSnake.ChangeDirection([direction]::North)}
+			"Down" {$Global:GameSnake.ChangeDirection([direction]::South)}
+			"Left" {$Global:GameSnake.ChangeDirection([direction]::West)}
+			"Right" {$Global:GameSnake.ChangeDirection([direction]::East)}			
+		}
+		Write-Debug $Global:GameSnake.Direction
+	}
+
+	Function Show-MessageBox {
+		param (
+			[string] $message,
+			[string] $title
+		)
+		[Windows.Forms.MessageBox]::Show("$message", "$title", [Windows.Forms.MessageBoxButtons]::OK, [Windows.Forms.MessageBoxIcon]::Information)
+	}
+
+#endregion
+
+#region Show the GUI
+
 $xamGUI.ShowDialog() | out-null
+
+#endregion
---
+++ Snake/Snake_class.ps1
@@ -0,0 +1,56 @@
+﻿Class Snake {
+	# Properties
+	[int]$Length
+	[direction]$Direction
+	[speed]$Speed
+
+	# Constructor - Paramterless
+	Snake () {
+		$this.Length = 0
+		$this.Direction = [direction]::Stationary
+		$this.Speed =  [speed]::very_slow
+	}
+
+	# Constructor	
+	Snake ([speed]$Speed) {
+		$this.Length = 0
+		$this.Direction = [direction]::Stationary
+		$this.Speed = $Speed
+	}
+
+	# Change Direction of the Snake
+	[Void] ChangeDirection([direction]$Direction) {
+		$this.Direction = $Direction
+	}
+
+	# Change Speed of the Snake 
+	[Void] ChangeSpeed([speed]$Speed) {
+		$this.Speed = $Speed
+	}
+
+	# Change Length of the Snake
+	[Void] ChangeLength([int]$Length) {
+		$this.Length = $Length
+	}
+
+	# Increment Length by one
+	[Void] IncrementLength() {
+		$this.Length += 1
+	}
+}
+
+Enum direction {
+	Stationary = 0
+	North = 1
+	East = 2
+	South = 3
+	West = 4
+}
+
+Enum speed {
+	very_slow = 0
+	slow = 1
+	normal = 2
+	fast = 3
+	ultra_fast = 4
+}
